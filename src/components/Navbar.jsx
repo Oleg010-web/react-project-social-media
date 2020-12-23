505875fef9a76c06857d54d660e86da4638243b0
Oleg010-web <nesteroff_oleg@mail.ru>
2020-12-23 20:14:54 +0300

    added css modules, added new personal css-files for each component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,19 @@
+import navBarModules from "./Navbar.module.css";
 const Navbar = () => {
-    return  <nav className="nav">
-    <div>
+    return  <nav className={navBarModules.nav}>
+    <div className={`${navBarModules.item} ${navBarModules.active}`}>
       <a>Profile</a>
     </div>
-    <div>
+    <div className={navBarModules.item}>
       <a>Messages</a>
     </div>
-    <div>
+    <div className={navBarModules.item}>
       <a>News</a>
     </div>
-    <div>
+    <div className={navBarModules.item}>
       <a>Music</a>
     </div>
-    <div>
+    <div className={navBarModules.item}>
       <a>Settings</a>
     </div>
   </nav>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,6 @@
+import profilecss from "./Profile.module.css";
 const Profile = () => {
-    return <div className="content">
+    return <div className={profilecss.content}>
     <div>
       <img src="https://www.forumdaily.com/wp-content/uploads/2016/06/Depositphotos_28019327_m-2015.jpg"/>
     </div>
@@ -11,11 +12,11 @@ const Profile = () => {
       <div>
         New posts
       </div>
-      <div>
-        <div>
+      <div className={profilecss.posts}>
+        <div className={profilecss.item}>
           Post 1
         </div>
-        <div>
+        <div className={profilecss.item}>
           Post 2
         </div>
       </div>

5f3b611e28b6887055c6400f835532560deb5be8
Oleg010-web <62741066+Oleg010-web@users.noreply.github.com>
2020-12-23 18:00:03 +0300

    Add files via upload
    My main files in project

---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+const Profile = () => {
+    return <div className="content">
+    <div>
+      <img src="https://www.forumdaily.com/wp-content/uploads/2016/06/Depositphotos_28019327_m-2015.jpg"/>
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      My posts
+      <div>
+        New posts
+      </div>
+      <div>
+        <div>
+          Post 1
+        </div>
+        <div>
+          Post 2
+        </div>
+      </div>
+    </div>
+  </div>
+}
+
+export default Profile;
